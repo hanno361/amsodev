a132c49e84e788f2b014a9d2b9080d97586cd167
hanno361 <kadir.atac@ogr.sakarya.edu.tr>
2023-05-20 13:34:57 +0300

    slider yapımı

---
+++ script.js
@@ -0,0 +1,8 @@
+const menubtn = document.querySelector(".menu-btn");
+const navigation = document.querySelector(".navigation");
+
+menubtn.addEventListener("click", () => {
+
+    menubtn.classList.toggle("active");
+    navigation.classList.toggle("active");
+});
